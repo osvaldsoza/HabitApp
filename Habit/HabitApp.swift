e4ea94e957aeee1c349768ddd5c69da8a6e3441b
Osvaldo Souza Ferreira <osvald.soza@gmail.com>
2023-07-04 15:46:05 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by osvaldo ferreira on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
